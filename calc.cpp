68d5bb3994ecd83de8c634683d56185cf5d35f21
yooseungkim <[yooseungkim@gm.gist.ac.kr]>
2022-11-14 17:10:28 +0900

    add: calc.cpp

---
+++ calc.cpp
@@ -0,0 +1,24 @@
+#include "calc.h"
+
+Calc::Calc()
+{
+}
+int Calc::add(int x, int y)
+{
+    return x + y;
+}
+
+int Calc::sub(int x, int y)
+{
+    return x - y;
+}
+
+int Calc::mul(int x, int y)
+{
+    return x * y;
+}
+
+double Calc::div(int x, int y)
+{
+    return x / y;
+}
---
+++ calc.h
@@ -0,0 +1,15 @@
+#ifndef ADD_H
+#define ADD_H
+
+class Calc
+{
+public:
+    Calc();
+
+    int add(int x, int y);
+    int sub(int x, int y);
+    int mul(int x, int y);
+    double div(int x, int y);
+};
+
+#endif
